07318b7314e407d40c6c1a2d25447cd78ac540b4
Jason Nguyen <jasonvex@gmail.com>
2017-03-22 21:25:36 -0500

    Adds Main page

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,4 +5,7 @@ import { render } from 'react-dom';
 // import css
 import css from './styles/style.styl';
 
-render(<p>hello</p>, document.getElementById('root'));
+// import components
+import Main from './components/Main';
+
+render(<Main/>, document.getElementById('root'));
